1daad4e29128807e71425fd3a830e46053d915e9
Marlon Buella <marlon.buella@movent.com>
2017-10-23 13:54:05 +0800

    Routing, controller and twig template for Yoda/Event

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,38 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+//JsonResponse library
+// use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($firstName,$count)
     {
-        return $this->render('EventBundle:Default:index.html.twig');
+		// dumping variables:
+		// dump($firstName,$count);die();
+		
+		// returning a Response object
+		// return new Response('It\'s a traaaaaaaap!');
+		
+		// returning a JSON response
+		// $arr = array(
+			// 'firstName' => $firstName,
+			// 'count' => $count,
+			// 'status' => 'It\'s a traaaaaaaap!',
+		// );
+		// return new Response(json_encode($arr));
+		// --or--
+		//return new JsonResponse($arr);
+		
+		// returning with template
+        return $this->render('EventBundle:Default:index.html.twig',
+			array(
+				'firstName' => $firstName,
+				'count' => $count,
+				'status' => 'It\'s a traaaaaaaap!',
+			)
+		);
+		
     }
 }
